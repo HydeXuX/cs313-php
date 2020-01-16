b4f6be0ef17739db65f0a3bfd8134129718382ad
HydeXuX <35950932+HydeXuX@users.noreply.github.com>
2020-01-15 21:16:17 -0700

    Create teamActivity.js

---
+++ web/week2team/teamActivity.js
@@ -0,0 +1,14 @@
+function changeColor(){
+    //inputColor = document.getElementById("color").value;
+    //document.getElementById("div1").style.backgroundColor = inputColor;backgroundColor
+    inputColor = $('#color').val();
+    $('#div1').css('backgroundColor', inputColor);
+}
+
+function clicked(){
+    alert("Clicked");
+}
+
+// $("#toggleJ").click(function(){
+//       $("#div3").fadeOut();
+//     });   

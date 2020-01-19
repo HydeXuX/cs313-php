fa830ea8a2ab3b4a8e0286ac1bfb8619beec18d2
HydeXuX <35950932+HydeXuX@users.noreply.github.com>
2020-01-18 18:48:35 -0700

    Update homepage.php

--- web/week02/homepage.php
+++ web/week02/homepage.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<p lang="en">
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="homepage.css">
 
+<head>
+    <title>Hyde Xu's Homepage</title>
+</head>
+
+<body>
+    <h1>Welcome to Hyde Xu's homepage</h1>
+    <h2>Introudction</h2>
+    <div>
+        <p>Welcome to my homepage. My name is Hyde Xu. I'm from Shanghai, China. It's one of the biggest cities in the world.</p>
+        <h3>Something about me</h3>
+        <p>I'm trilingual and I speak Mandarin Chinese, English and Japnese. I love learn languages since you can also discover a lot about their culture. I enjoy sketching and listening to music a lot. I also have passion in gaming.</p>
+        <h4>Here is a picture of one of my work I did in an art class</h4>
+        <img src="https://lh3.googleusercontent.com/pN8kMnnr6y8HGWqBt4C9e4-SVVtBoxsOPK0_FtTHw5SCdkx2kP7pwJE-Z05atBgQFwWuSoqZL16xtZOxhqgH77mAcKm_ghsQde1TtigX_fgN8J-2gVuzQQizfzNiM7DwWhrLUkCmS5t5O1DqmTIi9ifqbgDwxNnAsrizzQoOLh88BFOFSkwMInGNeIHsMBg2WUpXq2OljIv2kYWCzrJMW5FB-4kU1x5jxKFcBtXdBN9Sld9HgtbgyiNZ-hiQZd2BttfDju_IOqJ1_nmkgoame7NHJfU1oU3F69aIzJaesER1LE_tkfvhRqwhJiei9dk7fSP7CccE6-2qZ_cyF9Re79kFYxzQ6oGrmKMV_cIWhLKWNvxlSE1nQ11Ns4w6ij86TWDDSw1eX368BluDas-p-joL2G1RwXAEt2fSkcoOOK5sT_S-WomwEAtmIjFhOxcerVVJ3OG0J5HP_tBdvHo1k_nyouNOEXA977ocdVw_Pbu5pNdcYFLGvjztwMNxzaSHL5bPyVmEu4Aft2g1uagThVhFhwIdotjCuF2Bn3IfrROHj9r9ZwuOBA3JdBWRCQLqkksKapgScE_msCrBNdnsaDjaM7jzTsV-0L-Tl_U4UFad-Ih8BkuSzFtbBzrLw0rfDow6h22KGdNGM7K-y3VC-DXQqQg90KwQQmh0Z6HvGlXvGbxtmg24ew=w958-h720-no" alt="my drawing">    
+    </div>
+    <a href="assignmnets.html">Link to my assignments</a>
+    <?php echo "<p>Copyright &copy; 1998-2019" . date("Y") . " Hyde Xu</p>"; ?>
+</body>
